62067d9a40833e10a905c719a285f16463022f54
Bilal Goktas <bgoktas97@gmail.com>
2022-03-26 17:49:04 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from "apollo-server";
 
-import typeDefs from './schema';
-import resolvers from './resolvers';
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
 
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+  typeDefs: typeDefs,
+  resolvers: resolvers,
 });
 
-server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+server.listen(process.env.PORT || 5000).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
 });
